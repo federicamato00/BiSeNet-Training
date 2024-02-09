d5df9175d4e1855a4b0063eedb32e79008840611
federicamato00 <134133171+federicamato00@users.noreply.github.com>
2024-02-09 17:16:13 +0100

    Update bisenetv1.py

--- lib/models/bisenetv1.py
+++ lib/models/bisenetv1.py
@@ -315,7 +315,7 @@ class BiSeNetV1(nn.Module):
 
 
 if __name__ == "__main__":
-    net = BiSeNetV1(19)
+    net = BiSeNetV1(20)
     net.cuda()
     net.eval()
     in_ten = torch.randn(16, 3, 640, 480).cuda()
